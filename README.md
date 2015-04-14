d6157f6398c7452fe105bf1c4ca26ffcde0ca3b5
Luis Herranz <luisherranz@gmail.com>
2015-04-14 16:29:44 +0200

    Update github repo and add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# MeteorFlux Dispatcher
+
+I am still trying to figure out what is the best way to use Flux in Meteor, so this is still a work in progress.
+
+You can read about it in this forum post:
+https://forums.meteor.com/t/meteorflux-flow/920
--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'meteorflux:dispatcher',
-  version: '1.0.1',
+  version: '1.0.2',
   // Brief, one-line summary of the package.
   summary: 'A Flux Dispatcher for Meteor, based on the Facebook\'s Flux Dispatcher',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/MeteorFlux/Dispatcher.git',
+  git: 'https://github.com/meteorflux/dispatcher.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: null
+  documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
